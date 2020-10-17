1a11507ec63c70c311f0afb5b7b45d885deb2c1a
rexolboy8 <aspirineye@gmail.com>
2020-10-17 08:35:26 +0300

    Added the bot itself lol

---
+++ index.js
@@ -0,0 +1,99 @@
+// Load up the discord.js library
+const Discord = require("discord.js");
+
+/*
+ DISCORD.JS VERSION 12 CODE
+*/
+
+
+// This is your client. Some people call it `bot`, some people call it `self`, 
+// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
+// this is what we're refering to. Your client.
+const client = new Discord.Client();
+// Here we load the config.json file that contains our token and our prefix values. 
+const config = require("./config.json");
+const { SSL_OP_NETSCAPE_REUSE_CIPHER_CHANGE_BUG } = require("constants");
+// config.token contains the bot's token
+// config.prefix contains the message prefix.
+
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setActivity(`with your mom`);
+});
+
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+});
+
+
+client.on("message", async message => {
+  // This event will run on every single message received, from any channel or DM.
+  
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+  if(message.author.bot) return;
+  
+  // Also good practice to ignore any message that does not start with our prefix, 
+  // which is set in the configuration file.
+  if(!message.content.startsWith(config.prefix || config.prefix.toLowerCase() || config.prefix.toUpperCase())) return;
+  
+  // Here we separate our "command" name, and our "arguments" for the command. 
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  // Let's go with a few common example commands! Feel free to delete or change those.
+  
+  if(command === "ping") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+    
+  }
+  if(command === "say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete(); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+    
+  }
+  if(command === "oof") {
+    message.delete(); 
+    message.channel.send("fuck"+"you");
+    
+  }
+  if(command === "applause") { 
+    for (let i = 0; i < 9001; i++) {
+      message.channel.send("*applause*");
+    }
+  }
+
+ if(command === "ok")
+ {
+  message.channel.send("https://tenor.com/view/steinsgate-thumbs-up-nice-sweet-cool-gif-10332382");
+  message.delete();
+ }
+ if(command === "shut" && args.join(" ") === "the fuck up")
+ {
+  message.channel.send("ok then");
+  message.channel.send("i like pineapples");
+ }
+
+});
+
+client.login(config.token);
